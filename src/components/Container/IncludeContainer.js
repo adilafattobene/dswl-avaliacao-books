8b5e1565320afa992f7cd104c8eee2ccd9fa7c0c
afattobene <adila.fattobene@ifood.com.br>
2021-06-03 12:23:47 -0300

    feat: add rootContainer into includeContainer

--- src/components/Container/IncludeContainer.js
+++ src/components/Container/IncludeContainer.js
@@ -1,12 +1,13 @@
 import React from "react";
-import IncludeForm from "../Book/IncludeForm";
+import IncludeForm from "./Forms/IncludeForm";
+import RootContainer from "../Container/RootContainer";
 
 export default function IncludeContainer() {
   return (
-    <>
+    <RootContainer>
       <br />
       <br />
       <IncludeForm />
-    </>
+    </RootContainer>
   );
 }
