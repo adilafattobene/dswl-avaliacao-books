03511130d3412cd45a1654dbd238c6a0d736acf7
afattobene <adila.fattobene@ifood.com.br>
2021-06-03 16:01:56 -0300

    style: remove list bullets

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,17 +1,22 @@
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const List = styled.ul`
+  list-style: none;
+`;
 
 export default function Menu() {
   return (
     <header>
       <nav>
-        <ul>
+        <List>
           <li>
             <Link to="/consultar">|| Livros ||</Link>
             <Link to="/incluir"> Cadastrar ||</Link>
             <Link to="/deletar"> Deletar ||</Link>
             <Link to="/alterar"> Alterar ||</Link>
           </li>
-        </ul>
+        </List>
       </nav>
     </header>
   );
